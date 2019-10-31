1b38d44b31a50042a80ca3be16a6bdb34dc4fb91
Dave <davemers@iu.edu>
2019-10-30 23:01:17 -0400

    more playing around with individual layer outputs

--- src/test.py
+++ src/test.py
@@ -58,8 +58,26 @@ struct layer_struct{
 
 void init_net(const char *net_name);
 void run_net(unsigned char* input, unsigned int nr, unsigned int nc, unsigned int *res);
-void get_layer_01(struct layer_struct *data, const float** data_params);
+void get_layer_12(struct layer_struct *data, const float** data_params);
 ''')
+
+def jet_clamp(v):
+    v[v < 0] = 0
+    v[v > 1] = 1
+    return v
+
+def jet_color(t, t_min, t_max):
+
+    t_range = t_max - t_min
+    t_avg = (t_max + t_min) / 2.0
+    t_m = (t_max - t_avg) / 2.0
+
+    rgb = np.empty((t.shape[0], t.shape[1], 4), dtype=np.uint8)
+    rgb[:, :, 0] = (255*jet_clamp(1.5 - abs((4 / t_range)*(t - t_avg - t_m)))).astype(np.uint8)
+    rgb[:, :, 1] = (255*jet_clamp(1.5 - abs((4 / t_range)*(t - t_avg)))).astype(np.uint8)
+    rgb[:, :, 2] = (255*jet_clamp(1.5 - abs((4 / t_range)*(t - t_avg + t_m)))).astype(np.uint8)
+    rgb[:, :, 3] = np.full((t.shape[0], t.shape[1]), 255, dtype=np.uint8)
+    return rgb
 
 
 mnist_lib.init_net(weights_file.encode('utf-8'))
@@ -104,11 +122,34 @@ print(res[0])
 cm = color_img.tobytes()
 
 # Allocate the pointers passed to get_layer
-ls_01 = ffi.new('struct layer_struct*')
-ld_01 = ffi.new('float**')
-mnist_lib.get_layer_01(ls_01, ld_01)
+ls_12 = ffi.new('struct layer_struct*')
+ld_12 = ffi.new('float**')
+mnist_lib.get_layer_12(ls_12, ld_12)
 
-arr_out = np.frombuffer(ffi.buffer(ld_01[0], ls_01.size*4), dtype=np.float32)
+l12_out = np.frombuffer(ffi.buffer(ld_12[0], ls_12.size*4), dtype=np.float32)
+min_v = np.amin(l12_out)
+max_v = np.amax(l12_out)
+img_length = ls_12.nr * ls_12.nc
 
+l12_1 = np.reshape(l12_out[0:img_length], [ls_12.nr, ls_12.nc])
+l12_1 = cv.resize(l12_1, (20*ls_12.nr,20*ls_12.nc), interpolation=cv.INTER_NEAREST)
+
+
+l12_all = np.reshape(l12_out, [ls_12.nr, ls_12.nc, ls_12.k], order='F')
+
+t2 = []
+for idx in range(ls_12.k):
+    l12_1j = jet_color((l12_all[:,:,idx]).transpose(), min_v, max_v)
+    l12_1j = cv.resize(l12_1j, (20*ls_12.nr,20*ls_12.nc), interpolation=cv.INTER_NEAREST)
+    t2.append(cv.cvtColor(l12_1j, cv.COLOR_RGBA2BGRA))
+
+
+for idx in range(ls_12.k):
+    cv.imshow("test", t2[idx])
+    cv.waitKey(-1)
+
+cv.imshow("test2", l12_1)
+
+cv.waitKey(-1)
 
 bp = 1
